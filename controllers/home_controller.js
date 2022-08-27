2abbc2717ccd93fc39d897e3a26b5a1b7531ecc5
Radhakanta Swain <tata.swain@gmail.com>
2022-08-28 01:23:39 +0530

    show post data and user in the home screen

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,24 @@
+const { populate } = require('../models/post');
+const Post=require('../models/post');
+const { post } = require('../routes');
+
 module.exports.home=function(req,res){
-    console.log(req.cookies);
-    res.cookie('user_id',25);
-    return res.render('home',{
-        title:'home'
-    });
+    // console.log(req.cookies);
+    // res.cookie('user_id',25);
+    // Post.find({},function(err,posts){
+
+    //     return res.render('home',{
+    //         title:'home',
+    //         posts:posts
+    //     });
+    // });
+
+    //populate the user from user id from posts
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title:'home',
+            posts:posts
+        });
+    })
+    
 }
